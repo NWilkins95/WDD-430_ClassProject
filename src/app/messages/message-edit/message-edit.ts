f40e95f17b9b4d6c6d761cbc25779bb4da121fbf
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2026-01-22 16:11:20 -0700

    Enhance message editing functionality and improve message display in the UI

--- src/app/messages/message-edit/message-edit.ts
+++ src/app/messages/message-edit/message-edit.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-edit',
@@ -7,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './message-edit.css',
 })
 export class MessageEdit {
+  @ViewChild('msgText') msgTextInput: ElementRef<HTMLInputElement>;
+  @ViewChild('subject') subjectInput: ElementRef<HTMLInputElement>;
+  
+  @Output() addMessageEvent = new EventEmitter<Message>();
+  currentSender: string = 'Nicholas Wilkins';
 
+
+  onSendMessage() {
+    const msgText = this.msgTextInput.nativeElement.value;
+    const subject = this.subjectInput.nativeElement.value;
+
+    const newMessage = new Message("1", subject, msgText, this.currentSender);
+
+    this.addMessageEvent.emit(newMessage);
+  }
+
+  onClear() {
+    this.msgTextInput.nativeElement.value = '';
+    this.subjectInput.nativeElement.value = '';
+  }
 }
