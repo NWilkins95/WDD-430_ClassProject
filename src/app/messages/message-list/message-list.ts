561cc39bc5ad144623887c6b856c481667e0b001
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2026-01-22 16:28:17 -0700

    Refactor message list and edit components to enhance message handling and display

--- src/app/messages/message-list/message-list.ts
+++ src/app/messages/message-list/message-list.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Message } from '../message.model';
 
 @Component({
   selector: 'cms-message-list',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './message-list.css',
 })
 export class MessageList {
+  messages: Message[] = [
+    new Message('2', 'Hello World', 'This is Josh. Fear my wrath!', 'Josh Wilkins'),
+    new Message('3', 'Angular', 'Angular makes web development easier', 'Archer Wilkins'),
+    new Message('4', 'TypeScript', 'TypeScript adds types to JavaScript', 'Jannetta Wilkins'),
+  ]
 
+  onAddMessage(message: Message) {
+    this.messages.push(message);
+  }
 }
