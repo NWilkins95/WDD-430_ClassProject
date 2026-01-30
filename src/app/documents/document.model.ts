3c737056de704f6eff2f02773370e9ca03ffbcc4
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2026-01-29 21:39:20 -0700

    Refactor document model to rename 'title' to 'name' and update document item component for improved display

--- src/app/documents/document.model.ts
+++ src/app/documents/document.model.ts
@@ -1,13 +1,13 @@
 export class Document {
     public id: string;
-    public title: string;
+    public name: string;
     public description: string;
     public url: string;
     public children: Document[];
     
-    constructor(id: string, title: string, description: string, url: string, children: Document[] = []) {
+    constructor(id: string, name: string, description: string, url: string, children: Document[] = []) {
         this.id = id;
-        this.title = title;
+        this.name = name;
         this.description = description;
         this.url = url;
         this.children = children;
